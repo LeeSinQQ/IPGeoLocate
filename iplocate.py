a9975e4f3ca03ad5e059495451e715604a2d2c9e
iceQQ <42516265+iceQQ@users.noreply.github.com>
2018-08-21 16:18:00 +0300

    Update iplocate.py

--- iplocate.py
+++ iplocate.py
@@ -5,18 +5,19 @@ import urllib2
 import json
 
 
-while True:
-    ip = raw_input("ip address: ")
-    url = "https://ip-api.com/json/"
+def iplocate():
+    os.system("figlet IP-Locator")
+    print "\n"
+    ip = raw_input("IP or WebSite: ")
+    url = "http://ip-api.com/json/"
     response = urllib2.urlopen(url + ip)
     data = response.read()
     values = json.loads(data)
     os.system("clear")
-    os.system("figlet Scanning")
     print "\n"
     print "Query: ", values["query"]
     print "status: ", values["status"]
-    print "Country:", value["country"]
+    print "Country:", values["country"]
     print "City: ", values["city"]
     print "regionName: ", values["regionName"]
     print "timeZone: ", values["timezone"]
@@ -25,4 +26,5 @@ while True:
     print "zipcode: ", str(values["zip"])
     print "\n"
     print "EASY AS FUCK :P"
+iplocate()
 
